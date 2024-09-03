807280ff27d04abbf7976d82abab0303848bbc0b
Juri Sajzew <j.sajzew@outlook.de>
2024-09-03 12:01:06 +0200

    added docker

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -64,10 +64,10 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -205,6 +205,7 @@ CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379/1",
+        #"LOCATION": "redis://redis:6379/1",
         "OPTIONS": {
             "PASSWORD": 'foobared',
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
