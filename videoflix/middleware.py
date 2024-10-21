61bbb27663097c232034c27991010d20b59e1395
Juri Sajzew <j.sajzew@outlook.de>
2024-10-21 19:34:55 +0200

    update middleware.py

--- videoflix/middleware.py
+++ videoflix/middleware.py
@@ -1,3 +1,7 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 class ForwardedForMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -5,7 +9,8 @@ class ForwardedForMiddleware:
     def __call__(self, request):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
-            # Nimm die erste IP, die die tatsächliche Client-IP ist
-            request.META['REMOTE_ADDR'] = x_forwarded_for.split(',')[0].strip()
+            ip = x_forwarded_for.split(',')[0].strip()
+            logger.debug(f"Extracted IP: {ip}")  # Debug-Log hinzufügen
+            request.META['REMOTE_ADDR'] = ip
         response = self.get_response(request)
         return response
