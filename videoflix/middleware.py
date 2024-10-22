ba0dd23370ccdb956b2875d17808da600d9ffa5a
Juri Sajzew <j.sajzew@outlook.de>
2024-10-22 18:51:19 +0200

    update middleware

--- videoflix/middleware.py
+++ videoflix/middleware.py
@@ -9,8 +9,13 @@ class ForwardedForMiddleware:
     def __call__(self, request):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
+            # Nur die erste IP-Adresse extrahieren
             ip = x_forwarded_for.split(',')[0].strip()
-            logger.debug(f"Extracted IP: {ip}")  # Debug-Log hinzufügen
+            logger.info(f"Extracted IP: {ip}")  # Log-Level auf "info" setzen für Sichtbarkeit
             request.META['REMOTE_ADDR'] = ip
+        else:
+            logger.debug("No X-Forwarded-For header found")
+        
+        # Weiterleiten der Anfrage
         response = self.get_response(request)
         return response
