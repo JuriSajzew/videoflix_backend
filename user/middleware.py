dc52778055bfed099766d3dcd522d938b8bc29d9
Juri Sajzew <j.sajzew@outlook.de>
2024-10-24 09:17:29 +0200

    update middleware.py

--- user/middleware.py
+++ user/middleware.py
@@ -4,8 +4,7 @@ class RealIPMiddleware(MiddlewareMixin):
     def process_request(self, request):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
-            # X-Forwarded-For enthält eine Liste von IP-Adressen, getrennt durch Komma
-            ip = x_forwarded_for.split(',')[0]  # Nimm die erste IP-Adresse
+            ip = x_forwarded_for.split(',')[0]
         else:
-            ip = request.META.get('REMOTE_ADDR')  # Wenn kein X-Forwarded-For-Header vorhanden ist
-        request.META['REMOTE_ADDR'] = ip  # Setze die IP-Adresse neu
+            ip = request.META.get('REMOTE_ADDR')
+        request.META['REMOTE_ADDR'] = ip
